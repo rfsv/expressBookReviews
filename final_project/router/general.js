6285cb012631ed3a53295aa33482f7e4892b13a0
Rolando <152486791+rfsv@users.noreply.github.com>
2023-11-30 12:02:14 +0100

    Update register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here - Taks 6 - Work
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } else {
+    return res.status(404).json({message: "Unable to register user"});
+  }
 });
 
 // Get the book list available in the shop
