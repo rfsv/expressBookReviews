9070e2ce9a68272e17572e1533debdae98f442f1
rfsv <rsammarra@gmail.com>
2023-12-03 09:13:54 -0500

    Update tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,6 +21,7 @@ public_users.post("/register", (req,res) => {
   }
 });
 
+//-------------------------------------------------------------------------------------------------------
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here - Task 10 - 
@@ -36,6 +37,7 @@ public_users.get('/',function (req, res) {
       })
 });
 
+//-------------------------------------------------------------------------------------------------------
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here - Task 11 -
@@ -44,12 +46,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     if (books[isbn]){
       res.send(books[isbn]);
-      resolve("Response was sent - desde IF");
+      resolve("Response was sent");
     } else {
       res.send("ISBN not found");
       reject("ISBN not found")
     }
-
   })
   //Call the promise and wait for it to be resolved and then print a message.
   GetBookDetailsPromise
@@ -59,43 +60,77 @@ public_users.get('/isbn/:isbn',function (req, res) {
       console.log("From Callback catch " + ErrorMessage);});  
 });  
 
-
+//-------------------------------------------------------------------------------------------------------
 // Get book details based on author
 // Hints:
 // 1. Obtain all the keys for the ‘books’ object.
 // 2. Iterate through the ‘books’ array & check the author matches the one provided in the request parameters.
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here - Task 3 - Works
-  const req_author = req.params.author;
-  bookslist_array = books;
-  authorlist_array = {};
-  for(var key in bookslist_array) {
-      if(bookslist_array.hasOwnProperty(key)) {
-          var value = bookslist_array[key];
-          if  (value["author"] == req_author) {
-              authorlist_array[key]=value;
-          }
-      }
-  }
-  res.send(authorlist_array);
+    //Write your code here - Task 12
+    let GetBookDetailsByAuthorPromise = new Promise((resolve,reject) => {
+        const req_author = req.params.author;
+        bookslist_array = books;
+        authorlist_array = {};
+        for(var key in bookslist_array) {
+          if(bookslist_array.hasOwnProperty(key)) {
+              var value = bookslist_array[key];
+              if  (value["author"] == req_author) {
+                  authorlist_array[key] = value;
+              }
+            }  
+        }
+        if (!(Object.keys(authorlist_array).length === 0)) {
+        //if (authorlist_array){
+          console.log("The Author exists");
+          res.send(authorlist_array);
+          resolve("The author exists.");
+        } else {
+          console.log("Author does not exist");
+          res.send("The author does not exist.") 
+          reject("The author does not exist.")
+        }
+      })
+      GetBookDetailsByAuthorPromise
+       .then((successMessage) => {
+          console.log("From Callback -> " + successMessage)
+        })
+       .catch((ErrorMessage) => { 
+          console.log("From Callback catch -> " + ErrorMessage);
+        });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here - Task 4 - Works
-  const req_title=req.params.title;
-  bookslist_array=books;
-  titlelist_array={};
-  for(var key in bookslist_array) {
-      if(bookslist_array.hasOwnProperty(key)) {
-          var value=bookslist_array[key];
-          if  (value["title"] == req_title) {
-            titlelist_array[key]=value;
-          }
-      }
-  }
-  res.send(titlelist_array);
-});
+    //Write your code here - Task 13
+    let GetBookDetailsByTitlePromise = new Promise((resolve,reject) => {
+        const req_title = req.params.title;
+        bookslist_array = books;
+        titlelist_array = {};  
+        for(var key in bookslist_array) {
+            if(bookslist_array.hasOwnProperty(key)) {
+              var value = bookslist_array[key];
+              if  (value["title"] == req_title) {
+                titlelist_array[key] = value;
+              }
+            }
+        }
+        if (!(Object.keys(titlelist_array).length === 0)) {
+          console.log("The Title exists");
+          res.send(titlelist_array);
+          resolve("The title exists");
+        } else {
+          console.log("Title does not exist");
+          res.send("The title does not exist.") 
+          reject("The title does not exist.")
+        }
+      })
+      GetBookDetailsByTitlePromise
+        .then(
+         (successMessage) => {console.log("From Callback -> " + successMessage)
+         })
+        .catch(
+         (ErrorMessage) => {console.log("From Callback catch -> " + ErrorMessage);
+        }); });
 
 // Get book review
 public_users.get('/review/:isbn',function (req, res) {
